a4ab31119ce932969d9d1319bb9f462970ef8d47
Tomasz Scislo <tomasz@scislo.eu>
2016-08-21 20:01:19 +0200

    stylus added

--- src/app/boot.ts
+++ src/app/boot.ts
@@ -1,4 +1,6 @@
 import {bootstrap}    from '@angular/platform-browser-dynamic'
 import {AppComponent} from './components/app.component'
 
+require('../styl/index.styl');
+
 bootstrap(AppComponent);
--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -3,37 +3,38 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
 module.exports = {
-  entry: {
-    "vendor": "./src/app/vendor",
-    "app": "./src/app/boot"
-  },
-  output: {
-    path: 'bin',
-    filename: "[name].bundle.js"
-  },
-  resolve: {
-    extensions: ['', '.ts', '.js']
-  },
-  devtool: 'source-map',
-  module: {
-    loaders: [
-      {
-        test: /\.ts(x?)$/,
-        loader: 'awesome-typescript-loader'
-      }
+    entry: {
+        "vendor": "./src/app/vendor",
+        "app": "./src/app/boot"
+    },
+    output: {
+        path: 'bin',
+        filename: "[name].bundle.js"
+    },
+    resolve: {
+        extensions: ['', '.ts', '.js']
+    },
+    devtool: 'source-map',
+    module: {
+        loaders: [
+            {
+                test: /\.ts(x?)$/,
+                loader: 'awesome-typescript-loader'
+            },
+            {test: /\.styl$/, loader: 'style-loader!css-loader!stylus-loader'}
+        ]
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin(/* chunkName= */"vendor", /* filename= */"./vendor.bundle.js"),
+        new HtmlWebpackPlugin({
+            title: 'My fist Angular 2 app',
+            template: 'src/index.html',
+            hash: true
+        }),
+        new BrowserSyncPlugin({
+            host: 'localhost',
+            port: 3003,
+            server: {baseDir: ['bin']}
+        })
     ]
-  },
-  plugins: [
-    new webpack.optimize.CommonsChunkPlugin(/* chunkName= */"vendor", /* filename= */"./vendor.bundle.js"),
-    new HtmlWebpackPlugin({
-      title: 'My fist Angular 2 app',
-      template: 'src/app/index.html',
-      hash: true
-    }),
-    new BrowserSyncPlugin({
-      host: 'localhost',
-      port: 3003,
-      server: { baseDir: ['bin'] }
-    })
-  ]
 }
