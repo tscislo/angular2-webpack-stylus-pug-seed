a596966eb0fb4ae3ebd2bd9a818162bbd4522c8a
Tomasz Scislo <tomasz@scislo.eu>
2016-08-23 21:25:59 +0200

    dependecies update

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,40 +1,6 @@
 var webpack = require("webpack");
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+var WebpackConfig = require('webpack-config');
+var path = require('path');
+var absPath = (x) => path.join(__dirname, x);
 
-module.exports = {
-    entry: {
-        "vendor": "./src/app/vendor",
-        "app": "./src/app/boot"
-    },
-    output: {
-        path: 'bin',
-        filename: "[name].bundle.js"
-    },
-    resolve: {
-        extensions: ['', '.ts', '.js']
-    },
-    devtool: 'source-map',
-    module: {
-        loaders: [
-            {
-                test: /\.ts(x?)$/,
-                loader: 'awesome-typescript-loader'
-            },
-            {test: /\.styl$/, loader: 'style-loader!css-loader!stylus-loader'}
-        ]
-    },
-    plugins: [
-        new webpack.optimize.CommonsChunkPlugin(/* chunkName= */"vendor", /* filename= */"./vendor.bundle.js"),
-        new HtmlWebpackPlugin({
-            title: 'My fist Angular 2 app',
-            template: 'src/index.html',
-            hash: true
-        }),
-        new BrowserSyncPlugin({
-            host: 'localhost',
-            port: 3003,
-            server: {baseDir: ['bin']}
-        })
-    ]
-}
+module.exports = new WebpackConfig().extend(absPath('webpack.base.js')).merge({});
