acd37afe496d3a650c91f457095124688d6eaa0d
Tomasz Scislo <tomasz@scislo.eu>
2016-08-23 21:53:54 +0200

    0.0.2

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # angular2-webpack-stylus-pug-seed
+------------------------------------
+In this seed we incorporated the following technologies:
+* Angular2 RC5
+* TypeScript 1.8
+* Webpack 1.x
+* Pug
+* Stylus
--- webpack/webpack.build.js
+++ webpack/webpack.build.js
@@ -1,9 +1,17 @@
 var webpack = require("webpack");
 var WebpackConfig = require('webpack-config');
 var common = require('./common');
+var CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = new WebpackConfig().extend(common.absPath('webpack.base.js')).merge({
     plugins : [
+        new webpack.optimize.UglifyJsPlugin(
+            {
+                warning: false,
+                mangle: true,
+                comments: false
+            }
+        ),
         new CleanWebpackPlugin(['bin'], {
             root: process.cwd(),
             verbose: true
