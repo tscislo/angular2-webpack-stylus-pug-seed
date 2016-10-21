326c24cafb266c1bd52db995cef23f2362459392
Tomasz Scislo <tomasz@scislo.eu>
2016-10-21 21:10:16 +0200

    more standard loaders added

--- webpack/webpack.base.js
+++ webpack/webpack.base.js
@@ -26,7 +26,19 @@ module.exports = {
             },
             {
                 test: /\.pug|jade$/,
-                loader: 'pug-html-loader'
+                loader: 'html!pug-html-loader'
+            },
+            {
+                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+            },
+            {
+                test: /\.(ttf|eot|svg|png|gif|jpg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'file-loader'
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
             }
         ]
     },
--- webpack/webpack.build.js
+++ webpack/webpack.build.js
@@ -4,12 +4,18 @@ var common = require('./common');
 var CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = new WebpackConfig().extend(common.absPath('webpack.base.js')).merge({
-    plugins : [
+    htmlLoader: {
+        minimize: false // workaround for ng2
+    },
+    plugins: [
         new webpack.optimize.UglifyJsPlugin(
             {
                 warning: false,
                 mangle: true,
-                comments: false
+                comments: false,
+                compress: {
+                    warnings: false
+                }
             }
         ),
         new CleanWebpackPlugin(['bin'], {
